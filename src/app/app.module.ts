740fb857b3d4d3f19383f8749a855f2b38c4b52e
Napear <napear@gmail.com>
2020-03-31 21:23:55 -0700

    fixed spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FriendDashModule } from './friend-dash/friend-dash.module';
 
 
