d94d6a6906427ba4d1b0d4898c3752f38077420e
Napear <napear@gmail.com>
2020-03-31 21:45:02 -0700

    Add buttong added to lower left

--- src/app/friend-dash/friend-dash.component.ts
+++ src/app/friend-dash/friend-dash.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'beers-friend-dash',
   templateUrl: './friend-dash.component.html',
-  styles: [
+  styles: [`.fab-bottom-right {
+                    position: fixed;
+                    right: 35px;
+                    bottom: 35px;
+                    z-index: 100;
+          }`
   ],
 })
 export class FriendDashComponent implements OnInit {
--- src/app/friend-dash/friend-dash.module.ts
+++ src/app/friend-dash/friend-dash.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FriendDashComponent } from './friend-dash.component';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [FriendDashComponent],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
-    FriendDashComponent,
-    MatIconModule
+    FriendDashComponent
   ]
 })
 export class FriendDashModule { }
